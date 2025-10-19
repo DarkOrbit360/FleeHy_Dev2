df8329e3b9662b7d51731ee0f8a9b1828760a56b
DarkOrbit360 <fleehy6@gmail.com>
2025-10-19 19:17:06 +0530

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import Navbar from "./Navbar";
+
 
 export default function Layout({ title = "Fleehy", children }) {
   return (
@@ -9,7 +9,6 @@ export default function Layout({ title = "Fleehy", children }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <div className="min-h-screen bg-gray-50">
-        <Navbar />
         <main className="max-w-6xl mx-auto px-4 py-8">
           {children}
         </main>
